5899a25926a87aecc44762c814ec53ca3cd4ab00
medh64 <medhamedha826@gmail.com>
2025-01-30 16:13:06 +0100

    Springtime Dataset installation

---
+++ RDatasetInstallation.R
@@ -0,0 +1,4 @@
+install.packages("devtools", repos = "http://cran.us.r-project.org")
+devtools::install_github("bluegreen-labs/phenor", upgrade="never")
+devtools::install_github("ropensci/rppo", upgrade="never")
+install.packages(c("daymetr", "MODISTools", "phenocamr", "rnpn"), repos = "http://cran.us.r-project.org")
